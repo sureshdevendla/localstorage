db5972be50cdec3864f6a795fdffe80a9659576e
sureshdevendla <143864908+sureshdevendla@users.noreply.github.com>
2023-09-04 18:43:46 +0530

    Add files via upload

---
+++ milogin.js
@@ -0,0 +1,8 @@
+function add_details(){
+    var user_name=document.getElementById("name1").value;
+    localStorage.setItem("name of the user", user_name_name);
+    
+    var pass_word=document.getElementById("pwd1").value;
+    localStorage.setItem("the password is",pass_word);
+    window.location="mihome.html";
+}
